6465fe799e323fc9b0ad0782bb45d08c304772a7
Herman Sheremetyev <herman@swebpage.com>
2012-07-11 23:19:19 +0900

    Implement persistence between videos and clean things up

--- button.js
+++ button.js
@@ -1,69 +1,135 @@
 (function(){
-    var youtube = chrome.extension.connect({name: "udacity"});
-    youtube.onMessage.addListener(function(msg) {
-    });
-
     var speeds = [175, 150, 125, 100, 90, 80, 70];
 
-    var togglePopup = function(button) {
-      var o = document.getElementById('speed-button-menu');
-      o.style.display = (o.style.display == 'block' ? 'none' : 'block');
+    // Connect to the channel
+    var youtube = chrome.extension.connect({name: "udacity"});
+
+    // Retrieve current course from the URL
+    var currentCourse = function() {
+      return window.location.hash.replace(/Unit.*/, '');
     };
 
+    // localStorage getter wrapper
+    // Format: {course_id : {type1: speed, type2: speed}, ...}
+    // Leave some room to set different speeds for different
+    // lectures within the same course
+    // (some of the TA's talk *much* slower than the profs)
+    var getFromLocalStorage = function() {
+      var LS = JSON.parse(localStorage.getItem('playbackRate'));
+      if (LS && LS[currentCourse()]) {
+        return LS[currentCourse()]['main'];
+      }
+    };
+
+    // localStorage setter wrapper
+    var updateLocalStorage = function(speed) {
+      var LS = {};
+      LS[currentCourse()] = {main: speed};
+      localStorage.setItem('playbackRate', JSON.stringify(LS));
+    };
+
+    // Show/hide speed selection menu
+    var togglePopup = function(button) {
+      var menu = document.getElementById('speed-button-menu');
+      menu.style.display = (menu.style.display == 'block' ? 'none' : 'block');
+    };
+
+    // Send the update event to the playback content script,
+    // save to localStorage and update video speed display on the button.
+    // Things have a chance of getting out of sync here if the playback
+    // content script fails to set the speed, but it seems too cumbersome
+    // to set up the callback chain needed to verify it so we just
+    // plow ahead here.
     var updateSpeed = function(speed) {
+      youtube.postMessage({data: speed});
       var button = document.getElementById('speed-update-button');
       button.innerHTML = button.innerHTML.replace(/[0-9]*%/, '' + speed + '%');
-      var o = document.getElementById('speed-button-menu');
-      o.style.display = (o.style.display == 'block' ? 'none' : 'block');
+      updateLocalStorage(speed);
+      togglePopup();
     };
 
-    var buildOverlay = function(b) {
-      var outer = document.createElement('div');
-      outer.style.position = "relative";
-      outer.onclick = function(e) { e.stopPropagation(); };
-      b.parentNode.appendChild(outer);
-      var o = document.createElement('div');
-      o.id = 'speed-button-menu';
-      o.style.position = "absolute";
-      o.style.right = "155px";
-      o.style.top = "-" + (26 * speeds.length + 1) + "px";
-      o.style.width = "98px";
-      o.style.zIndex = "1000";
-      o.style.backgroundColor = 'gray';
-      o.style.display = "none";
+    // Send video speed info to the playback content script
+    // The delay is necessary as the video tag may not be fully
+    // loaded yet when the URL is updated. It's tricky as there
+    // is an existing video there so if we update too soon we end
+    // up updating the old one right before it's swapped out.
+    // TODO: make this more robust as the 2sec timeout may not always work
+    //       onChange for the video tag?
+    var speedPingCallback = function(msg) {
+      setTimeout(function() {
+        var speed = getFromLocalStorage();
+        if (speed != msg.data) {
+          youtube.postMessage({data: speed ? speed : 100});
+        }
+      }, 2000);
+    };
+
+    // Set up the menu overlay
+    var buildMenuOverlay = function(button) {
+      // Generate the outer container
+      var container = document.createElement('div');
+      // can't do this with CSS, position relative/absolute relationship
+      // with the inner div doesn't seem to get set up properly unless
+      // this is set directly :/
+      container.style.position = 'relative';
+      button.parentNode.appendChild(container);
+
+      // Generate the actual menu. We want to float it above the button
+      // while not being part of the button, hence all the round-aboutedness
+      var menu = document.createElement('div');
+      menu.id = 'speed-button-menu';
+      menu.style.top = "-" + (26 * speeds.length + 1) + "px";
+
+      // Generate all the menu entries
       for (var i = 0; i < speeds.length; i++) {
-        o.innerHTML += '<div class="button" style="width:98px;" onclick="(' + updateSpeed + ')(' + speeds[i] + '); return false;">' + speeds[i] + "%</div>";
+        var entry = document.createElement('div');
+        entry.className = 'button';
+        entry.style.width = '98px';
+        entry.innerHTML = '' + speeds[i] + '%';
+        entry.addEventListener('click', function(e) {
+          var speed = this.innerHTML.match(/[0-9]+/);
+          if (speed) { updateSpeed(speed); }
+          e.stopPropagation();
+        }, false);
+        menu.appendChild(entry);
       }
-      outer.appendChild(o);
+      container.appendChild(menu);
     };
 
+    // Generate the speed toggle button, immitating the autoplay button
     var buildButton = function(a){
-      var b = document.createElement('div');
-      b.id = 'speed-update-button'; 
-      b.innerHTML = "Speed: <strong>100%</strong>";
-      b.style.paddingLeft = "5px";
-      b.style.margin = "2px 0px 0px 22px";
-      b.style.width = "98px";
-      b.className = 'button gray-button';
-      b.onclick = function(e) { e.stopPropagation(); togglePopup(b); };
-      b.addEventListener('DOMSubtreeModified', function() {
-          setTimeout(function() {
-            youtube.postMessage({data: b.innerHTML.match(/[0-9]+/)});
-          }, 0); },
-          false);
-      a.parentNode.insertBefore(b, a);
+      var button = document.createElement('div');
+      var speed = getFromLocalStorage();
+      button.id = 'speed-update-button'; 
+      button.innerHTML = "Speed: <strong>" + (speed ? speed : "100") +"%</strong>";
+      button.className = 'button gray-button';
+      button.addEventListener('click', function(e) {
+        togglePopup(button);
+        e.stopPropagation();
+      }, false);
+      a.parentNode.insertBefore(button, a);
       return a;
     };
 
+    // Update video speed when the URL changes
+    window.addEventListener('popstate', speedPingCallback);
+
+    // Set callback to send video speed to the playback content script
+    youtube.onMessage.addListener(speedPingCallback);
+
+    // wait until the autoplay button has loaded in order to get
+    // the right positioning info
     var id_ = setInterval(function() {
       var buttons = document.getElementsByClassName('button gray-button');
       if (buttons.length) {
+        // yay, the autoplay button is here, kill the interval
         clearInterval(id_);
         delete id_;
-        var a = buttons[0];
-        a.style.width = "98px"; // make some space for the other button
-        var b = buildButton(a);
-        buildOverlay(b);
+        var autoPlayButton = buttons[0];
+        // make some space for the other button
+        autoPlayButton.style.width = "98px";
+        var button = buildButton(autoPlayButton);
+        buildMenuOverlay(button);
       }
     }, 1000);
 })();
--- playback.js
+++ playback.js
@@ -1,9 +1,25 @@
 (function() {
+    // connect to the channel
     var port = chrome.extension.connect({name: "youtube"});
-    port.onMessage.addListener(function(msg) {
+
+    // handle speed update messages
+    var updateSpeed = function(msg) {
+      if (!msg.data) return;
       var videos = document.getElementsByTagName('video');
-      if (videos.length && msg.data) {
+      if (videos.length) {
         videos[0].playbackRate = parseInt(msg.data) / 100;
       }
-    });
+    };
+    port.onMessage.addListener(updateSpeed);
+
+    // Send initial speed ping request along with current speed
+    setTimeout(function(){
+      var videos = document.getElementsByTagName('video');
+      if (videos.length) {
+        var currentSpeed = videos[0].playbackRate;
+        // assume 100% if we didn't get anything
+        currentSpeed = currentSpeed ? parseFloat(currentSpeed) * 100 : 100;
+        port.postMessage({data: currentSpeed});
+      }
+    }, 1000);
 })();
