a3d39dd5c67e8b525bb69c9614dec18a5b060f9f
Herman Sheremetyev <herman@swebpage.com>
2012-07-17 22:19:25 +0900

    Run background script at the top level

--- channel.js
+++ channel.js
@@ -1,32 +1,30 @@
 /*
  * Shuffle messages between udacity and youtube content scripts
  */
-(function() {
-    var udacity_ports = [];
-    var youtube_ports = [];
+var udacity_ports = [];
+var youtube_ports = [];
 
-    var handleConnection = function(port, from, to) {
-      var idx = from.length;
-      from[idx] = port;
-      port.onMessage.addListener(function(msg) {
-        if (to.length > idx) {
-          to[idx].postMessage(msg);
-        }
-      });
-      port.onDisconnect.addListener(function() {
-        from.splice(idx, 1);
-      });
-    };
+var handleConnection = function(port, from, to) {
+  var idx = from.length;
+  from[idx] = port;
+  port.onMessage.addListener(function(msg) {
+    if (to.length > idx) {
+      to[idx].postMessage(msg);
+    }
+  });
+  port.onDisconnect.addListener(function() {
+    from.splice(idx, 1);
+  });
+};
 
-    chrome.extension.onConnect.addListener(function(port) {
-      try {
-        if (port.name.match(/^udacity/) && !udacity_ports.indexOf(port) >= 0) {
-          handleConnection(port, udacity_ports, youtube_ports);
-        } else if (port.name.match(/^youtube/) && !youtube_ports.indexOf(port) >= 0) {
-          handleConnection(port, youtube_ports, udacity_ports);
-        }
-      } catch (e) {
-        alert('Udacity Speed Extension background page error: ' + e);
-      }
-    });
-})();
+chrome.extension.onConnect.addListener(function(port) {
+  try {
+    if (port.name.match(/^udacity/) && !udacity_ports.indexOf(port) >= 0) {
+      handleConnection(port, udacity_ports, youtube_ports);
+    } else if (port.name.match(/^youtube/) && !youtube_ports.indexOf(port) >= 0) {
+      handleConnection(port, youtube_ports, udacity_ports);
+    }
+  } catch (e) {
+    alert('Udacity Speed Extension background page error: ' + e);
+  }
+});
